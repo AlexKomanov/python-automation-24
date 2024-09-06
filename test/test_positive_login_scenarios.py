6c825c8343af327528868ec17d3e92235c7734de
AlexKomanov <akomanov88@gmail.com>
2024-09-06 23:53:40 +0300

    Refactoring of Products page

--- test/test_positive_login_scenarios.py
+++ test/test_positive_login_scenarios.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.common.by import By
 from pages.login_page import LoginPage
-
+from pages.products_page import ProductsPage
 
 def test_standard_user_login(browser):
     login_page = LoginPage(browser)
@@ -14,10 +14,6 @@ def test_standard_user_login(browser):
     login_page.click_login_button()
 
     # Validation of redirection
-    current_url = browser.current_url
-    print(f"{current_url = }")
-    assert current_url == "https://www.saucedemo.com/inventory.html"
-
-    products_page_title = browser.find_element(By.XPATH, '//*[@data-test="title"]').text
-    print(f"{products_page_title = }")
-    assert products_page_title == "Products"
+    products_page = ProductsPage(browser=browser)
+    products_page.validate_products_page_title()
+    products_page.validate_products_page_url()
